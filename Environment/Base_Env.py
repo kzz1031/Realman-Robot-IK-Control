80fbc57215d43308c027d643ec166e137919c051
Jingkai Wang <jkwang22@m.fudan.edu.cn>
2025-10-09 17:04:23 +0800

    add: class Base_Env

--- Environment/Base_Env.py
+++ Environment/Base_Env.py
@@ -1,19 +1,36 @@
 from isaacsim.simulation_app import SimulationApp
-simulation_app = SimulationApp({"headless": False})
+# simulation_app = SimulationApp({"headless": False})
 
 import numpy as np
 from isaacsim.core.api.objects import GroundPlane
 from isaacsim.core.api import World
+from isaacsim.core.utils.stage import add_reference_to_stage
 import omni.replicator.core as rep
-
 
 class BaseEnv:
     def __init__(self):
         self.world = World(stage_units_in_meters=1.0)
         self.world.scene.add(GroundPlane(prim_path="/World/Ground", name="Ground", color=np.array([0.2, 0.2, 0.2])))
         self.demo_light = rep.create.light(position=[0, 0, 0], light_type="dome")
+
+        self.realman_usd_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/overseas_75_b_v_description/overseas_75_b_v_description_lockplatform.usd"
+        self.realman_urdf_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/realman/rmc_aida_l_ros1-develop2/rmc_aida_l_ros1-develop/src/dual_arm_control/dual_arm_description/overseas_75_b_v_description/urdf/overseas_75_b_v_description.urdf"
+        self.realman_discription_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/Lula/realman.yaml"
+        
+        add_reference_to_stage(
+            usd_path=self.realman_usd_path,
+            prim_path="/World/Realman"
+        )
+
     def reset(self):
         self.world.reset()
 
     def step(self):
-        self.world.step()
+        self.world.step(render=True)
+
+# if __name__ == "__main__":
+#     env = BaseEnv()
+#     for _ in range(1000):
+#         env.step()
+
+# simulation_app.close()
--- test.py
+++ test.py
@@ -3,7 +3,6 @@ simulation_app = SimulationApp({"headless": False})
 
 from isaacsim.core.api.objects import GroundPlane
 from isaacsim.core.api.robots import Robot
-from isaacsim.core.utils.stage import add_reference_to_stage
 import omni.replicator.core as rep
 import omni.usd
 import numpy as np
@@ -14,41 +13,28 @@ from isaacsim.robot_motion.motion_generation import KinematicsSolver
 from isaacsim.robot_motion.motion_generation import ArticulationKinematicsSolver, LulaKinematicsSolver
 from termcolor import cprint
 from Kinematics.Kinematics_Solver import RealmanKinematicsSolver
+from Environment.Base_Env import BaseEnv
 
 
-world = World(stage_units_in_meters=1.0)
-rep.create.light(position=[0, 0, 0], light_type="dome")
-# 4. 添加地面
-world.scene.add(GroundPlane(prim_path="/World/Ground", name="Ground", color=np.array([0, 0, 0])))
+base_env = BaseEnv()
 
-# 5. 导入 Realman 机械臂 USD 文件
-#realman_usd_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/realman/rmc_aida_l_ros1-develop2/rmc_aida_l_ros1-develop/src/dual_arm_control/dual_arm_description/overseas_75_b_v_description/urdf/overseas_75_b_v_description/overseas_75_b_v_description.usd"
-realman_usd_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/overseas_75_b_v_description/overseas_75_b_v_description_lockplatform.usd"
-realman_urdf_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/realman/rmc_aida_l_ros1-develop2/rmc_aida_l_ros1-develop/src/dual_arm_control/dual_arm_description/overseas_75_b_v_description/urdf/overseas_75_b_v_description.urdf"
-realman_discription_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/Lula/realman.yaml"
-
-add_reference_to_stage(
-    usd_path=realman_usd_path,
-    prim_path="/World/Realman"
-)
 realman = Robot(
     prim_path="/World/Realman",
     name="realman",
-    position=(0, 0, 0.7),
-    orientation=(1, 0, 0, 0)  # 四元数 (w,x,y,z)
 )
-
+base_env.world.scene.add(realman)
 realman_articulation = SingleArticulation("/World/Realman")
-# 6. 初始化场景
-world.reset()
+
+base_env.reset()
+realman.set_world_pose(position=(0, 0, 0.2), orientation=(1, 0, 0, 0) )
 realman.initialize()
 realman_articulation.initialize()
 # cprint(f"Articulation is initialized: {realman_articulation.is_initialized}", "blue")
 cprint(f"realman base position: {realman.get_world_pose()[0]} orientation: {realman.get_world_pose()[1]}", "blue")
 
 realman_kinematics_solver = RealmanKinematicsSolver(
-    realman_description_path=realman_discription_path,
-    urdf_path=realman_urdf_path,
+    realman_description_path=base_env.realman_discription_path,
+    urdf_path=base_env.realman_urdf_path,
     realman_articulation=realman_articulation
 )
 
@@ -65,10 +51,8 @@ step = 0
 while True:
     # print(ur10.get_joint_positions())
     # step += 1
-    world.step(render=True)
-    if step % 10000 == 0:
-        realman_kinematics_solver.update(realman.get_world_pose()[0], realman.get_world_pose()[1])
+    base_env.step()
+    # if step % 10000 == 0:
+    #     realman_kinematics_solver.update(realman.get_world_pose()[0], realman.get_world_pose()[1])
         
-
-# 7. 关闭应用
 simulation_app.close()
