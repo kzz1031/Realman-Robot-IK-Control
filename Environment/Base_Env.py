ce568e72628058087cd6f38493386a6c2bd4f53c
Jingkai Wang <jkwang22@m.fudan.edu.cn>
2025-10-29 10:41:55 +0800

    add: table in BaseEnv

--- Environment/Base_Env.py
+++ Environment/Base_Env.py
@@ -6,21 +6,33 @@ from isaacsim.core.api.objects import GroundPlane
 from isaacsim.core.api import World
 from isaacsim.core.utils.stage import add_reference_to_stage
 import omni.replicator.core as rep
-
+from isaacsim.core.prims import SingleXFormPrim
 class BaseEnv:
     def __init__(self):
         self.world = World(stage_units_in_meters=1.0)
         self.world.scene.add(GroundPlane(prim_path="/World/Ground", name="Ground", color=np.array([0.2, 0.2, 0.2])))
         self.demo_light = rep.create.light(position=[0, 0, 0], light_type="dome")
 
-        self.realman_usd_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/overseas_75_b_v_description/overseas_75_b_v_description_lockplatform.usd"
-        self.realman_urdf_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/realman/rmc_aida_l_ros1-develop2/rmc_aida_l_ros1-develop/src/dual_arm_control/dual_arm_description/overseas_75_b_v_description/urdf/overseas_75_b_v_description.urdf"
-        self.realman_discription_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/Lula/realman.yaml"
-        
+        # self.realman_usd_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/overseas_75_b_v_description/overseas_75_b_v_description_lockplatform2.usd"
+        self.realman_usd_path = "/home/kzzwang/Documents/code/DigitTwin/Assets/overseas_75_b_v_description/realman_with_pads_cameras.usd"
         add_reference_to_stage(
             usd_path=self.realman_usd_path,
             prim_path="/World/Realman"
         )
+
+        self.table = SingleXFormPrim(
+            prim_path="/World/Table",
+            name="Table",
+            position=np.array([-0.1, -0.85, 0.12]),
+            scale=np.array([1.0, 1.0, 1.0]),
+            orientation=np.array([1.0, 0.0, 0.0, 0.0]),
+        )
+
+        add_reference_to_stage(
+            usd_path="/home/kzzwang/Documents/code/DigitTwin/Assets/assembly.usd",
+            prim_path="/World/Table"
+        )
+        
 
     def reset(self):
         self.world.reset()
@@ -30,7 +42,7 @@ class BaseEnv:
 
 # if __name__ == "__main__":
 #     env = BaseEnv()
-#     for _ in range(1000):
+#     while True:
 #         env.step()
 
 # simulation_app.close()
